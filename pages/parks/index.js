7668a92e14f6b479d4a587a690273771964e4e4b
Adrian Ricken <adrianricken@gmail.com>
2024-10-04 09:49:48 +0200

    sort list of parks alphabetical

--- pages/parks/index.js
+++ pages/parks/index.js
@@ -1,6 +1,5 @@
 import useSWR from "swr";
 import Card from "@/components/Card";
-import Link from "next/link";
 import styled from "styled-components";
 
 const ParkListItem = styled.li`
@@ -10,10 +9,12 @@ const ParkListItem = styled.li`
 export default function Parks() {
   const { data } = useSWR("/api/parks", { fallbackData: [] });
 
+  const sortedParks = data.sort((a, b) => a.name.localeCompare(b.name));
+
   return (
     <>
       <ul>
-        {data.map((park) => {
+        {sortedParks.map((park) => {
           return (
             <ParkListItem key={park._id}>
               <Card name={park.name} image={park.imageURL} />
